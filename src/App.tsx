186fd014855adee657769394c4eda72bad20ac9c
rina-mercury <ri.mercury@icloud.com>
2022-06-06 17:27:25 +0400

    Fix web versions, removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
-import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+export default function App() {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <Header />
-      </ScrollView>
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    >
+      <Text>Gotcha</Text>
     </SafeAreaView>
   );
 }
-
-export default App;
